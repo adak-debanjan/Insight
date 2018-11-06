a1a703313b8c9db200e363accab3644880aa7e70
adak-debanjan <debanjan.adak@gmail.com>
2018-11-05 23:08:37 -0500

    Updated Code and ReadMe
    Updated on 11/05/2018

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-Insight
+Problem
+-----------------------------------
+The purpose of this program is to get get the immigration data trends on H1B(H-1B, H-1B1, E-3) visa application processing over the past years and 
+to identify the occupations and states with the most number of approved H1B visas.
+
+This program takes the H1B data as input calculates two metrics: Top 10 Occupations and Top 10 States for certified visa applications.
+
+The program is modular and reusable for future. If the newspaper gets data for the year 2019 (with the assumption that the necessary data to calculate the metrics are available) 
+and puts it in the input directory, running the run.sh script should produce the results in the output folder without needing to change the code.
+
+
+Approach
+-----------------------------------
+Since each year the input data file can have different columns, we have used file names ColumnStructure.txt which will be placed in the input directory.
+This file will contain the column names used to calculate the metrics in ";" separated format. If there is any change in column names in the input data file, user needs to
+update this ColumnStructure.txt file. For example, in the 2015 and 2016 col_SOC_CODE and col_SOC_NAME were given by SOC_CODE and SOC_NAME but in year 2014, these two columns were
+given by LCA_CASE_SOC_CODE and LCA_CASE_SOC_NAME.
+
+The program will generate two output txt file for two metrics: top_10_occupations.txt and top_10_states.txt. The records in both the files are sorted by NUMBER_CERTIFIED_APPLICATIONS field, 
+and in case of a tie, alphabetically by TOP_OCCUPATIONS and TOP_STATES.
+
+Depending on the input, there may be fewer than 10 lines in each file. There, however, should not be more than 10 lines in each file. 
+In case of ties, only the top 10 records are listed based on the sorting methods given above.
+
+Percentages are rounded off to 1 decimal place. For instance, 1.05% is rounded to 1.1% and 1.04% is rounded to 1.0%. Also, 1% is represented by 1.0%
+
+Run Instruction
+------------------------------------
+In Unix system, the user needs to run the run.sh file to run the python source code (h1b_counting.py).
+In Windows system, the user needs to run the h1b_counting.py file directly in the src directory.
+
+The program will automatically take the file from input folder generate outputs in the output folder. The input file names should not be changed.
+The input files are: h1b_input.csv and ColumnStructure.txt
--- src/h1b_counting.py
+++ src/h1b_counting.py
@@ -1,53 +1,66 @@
+#Input file for H1B data
 input_file=open('../input/h1b_input.csv',encoding="utf8")
+
+#Input file for the column structure of the H1B data. This file contains only the relevant column names.
 structure_file=open('../input/ColumnStructure.txt',encoding="utf8")
 
+#Extract the input ";" separated H1B data and store it in a list
 F_List = []
 for line in input_file:
     F_List.append(line.split(';'))
 
+#Extract the input ";" separated column structure data and store it in a list
 col_List = []
 for line in structure_file:
     col_List.append(line.strip().split(';'))
 
+#Get the positions of the relevant column names in the column structure data
 col_idx1 = col_List[0].index('col_SOC_CODE')
 col_idx2 = col_List[0].index('col_SOC_NAME')
 col_idx3 = col_List[0].index('col_WORKSITE_STATE')
 col_idx4 = col_List[0].index('col_CASE_STATUS')
 
+#Get the actual column names from the column structure data based on the positions
 col_code = col_List[1][col_idx1]
 col_name = col_List[1][col_idx2]
 col_state = col_List[1][col_idx3]
 col_status = col_List[1][col_idx4]
 
+#Get the positions of the relevant column names in the input H1B data
 idx1 = F_List[0].index(col_code)
 idx2 = F_List[0].index(col_name)
 idx3 = F_List[0].index(col_state)
 idx4 = F_List[0].index(col_status)
 
-##Count of State Start
-      
+#############################################Counting the top 10 States##################################################
+
+#Get all the states in a list where the applications have been certified      
 cert_state = []
 for n in range(1,len(F_List)):
     if(F_List[n][idx4].strip().upper()=='CERTIFIED'):
         if(F_List[n][idx3] not in cert_state):
             cert_state.append(F_List[n][idx3])
 
+#Create a dictionary where keys are the states and values are lists containing number of applications and percentage of applications
 cert_dict = {item:[0,0] for item in cert_state}
 for n in range(1,len(F_List)):
     if(F_List[n][idx3] in cert_state):
         key=F_List[n][idx3]
         cert_dict[key][0] += 1
 
+#Get the total number of certified applications irrespective of the states
 app_total = 0
 for n in range(1,len(F_List)):
     if(F_List[n][idx4].strip().upper()=='CERTIFIED'):
         app_total += 1
 
+#Update the perecntage of applications for each states in the dictionary
 for n in range(1,len(F_List)):
     key=F_List[n][idx3]
     if(key in cert_state):
         cert_dict[key][1] = (cert_dict[key][0])/app_total
 
+#Write the data in output file
 cnt = 0
 output_file=open('../output/top_10_states.txt','w')
 output_file.write('TOP_STATES;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE\n')
@@ -58,8 +71,9 @@ for key, value in sorted(cert_dict.items(), key=lambda item: (-item[1][0],item[0
         break
 output_file.close()
 
-##Count of Occupation Start
-        
+#############################################Counting the top 10 Occupations##################################################
+
+#Get all the SOC Codes and the corresponding names in lists where the applications have been certified         
 cert_occp_code = []
 cert_occp_name = []
 for n in range(1,len(F_List)):
@@ -68,18 +82,21 @@ for n in range(1,len(F_List)):
             cert_occp_code.append(F_List[n][idx1])
             cert_occp_name.append(F_List[n][idx2].replace('"',''))
 
+#Create a dictionary where keys are the SOC codes and values are lists containing SOC names, number of applications and percentage of applications
 cert_dict = {item:['',0,0] for item in cert_occp_code}
 for n in range(1,len(F_List)):
     if(F_List[n][idx1] in cert_occp_code):
         key=F_List[n][idx1]
         cert_dict[key][1] += 1
         cert_dict[key][0] = F_List[n][idx2]
-        
+
+#Update the perecntage of applications for each SOC code in the dictionary        
 for n in range(1,len(F_List)):
     key=F_List[n][idx1]
     if(key in cert_occp_code):
         cert_dict[key][2] = (cert_dict[key][1])/app_total
 
+#Write the data in output file
 cnt = 0
 output_file=open('../output/top_10_occupations.txt','w')
 output_file.write('TOP_OCCUPATIONS;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE\n')
